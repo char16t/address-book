f64903dcb6502f5b65346ec6b2a4430af2ecfa14
Valeriy Manenkov <v.manenkov@gmail.com>
2016-03-23 15:36:57 +0300

    Fix CRLF line endings

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ cd /vagrant/address-book
 mvn package
 sudo cp ear/target/addressbook-ear-1.0.ear /opt/wildfly/standalone/deployments
 sudo touch /opt/wildfly/standalone/deployments/addressbook-ear-1.0.ear.dodeploy
+
