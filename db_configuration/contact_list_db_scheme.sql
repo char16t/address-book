bcc686bf8a2931d5de6edf1d9e65b140559bbaab
Valeriy Manenkov <v.manenkov@gmail.com>
2016-03-24 12:30:33 +0300

    Add entities to DB: user, roles. permissions

--- db_configuration/contact_list_db_scheme.sql
+++ db_configuration/contact_list_db_scheme.sql
@@ -1,4 +1,118 @@
 -- -----------------------------------------------------
+-- Table `mydb`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS "user";
+
+CREATE TABLE IF NOT EXISTS "user" (
+  id INT NOT NULL,
+  email VARCHAR(45) NULL,
+  password VARCHAR(45) NULL,
+  PRIMARY KEY (id))
+;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`role`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS "role";
+
+CREATE TABLE IF NOT EXISTS role (
+  id INT NOT NULL,
+  name VARCHAR(45) NULL,
+  PRIMARY KEY (id))
+;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`permission`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS permission ;
+
+CREATE TABLE IF NOT EXISTS permission (
+  id INT NOT NULL,
+  name VARCHAR(45) NULL,
+  PRIMARY KEY (id))
+;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`user_role`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS user_role ;
+
+CREATE TABLE IF NOT EXISTS user_role (
+  user_id INT NOT NULL,
+  role_id INT NOT NULL,
+  PRIMARY KEY (user_id, role_id)
+ ,
+  CONSTRAINT fk_user_has_role_user1
+    FOREIGN KEY (user_id)
+    REFERENCES "user" (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_user_has_role_role1
+    FOREIGN KEY (role_id)
+    REFERENCES role (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+CREATE INDEX fk_user_has_role_role1_idx ON user_role (role_id ASC);
+CREATE INDEX fk_user_has_role_user1_idx ON user_role (user_id ASC);
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`role_permission`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS role_permission ;
+
+CREATE TABLE IF NOT EXISTS role_permission (
+  role_id INT NOT NULL,
+  permission_id INT NOT NULL,
+  PRIMARY KEY (role_id, permission_id)
+ ,
+  CONSTRAINT fk_role_has_permission_role1
+    FOREIGN KEY (role_id)
+    REFERENCES role (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_role_has_permission_permission1
+    FOREIGN KEY (permission_id)
+    REFERENCES permission (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+CREATE INDEX fk_role_has_permission_permission1_idx ON role_permission (permission_id ASC);
+CREATE INDEX fk_role_has_permission_role1_idx ON role_permission (role_id ASC);
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`person_user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS person_user ;
+
+CREATE TABLE IF NOT EXISTS person_user (
+  person_id INT NOT NULL,
+  user_id INT NOT NULL,
+  PRIMARY KEY (person_id, user_id)
+ ,
+  CONSTRAINT fk_person_has_user_person1
+    FOREIGN KEY (person_id)
+    REFERENCES person (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_person_has_user_user1
+    FOREIGN KEY (user_id)
+    REFERENCES user (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+CREATE INDEX fk_person_has_user_user1_idx ON person_user (user_id ASC);
+CREATE INDEX fk_person_has_user_person1_idx ON person_user (person_id ASC);
+
+-- -----------------------------------------------------
 -- Table `addressbook`.`test`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS test ;
