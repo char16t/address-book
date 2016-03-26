b60e701359d46a3561b7f01e8bb9dfb73834dfcc
Valeriy Manenkov <v.manenkov@gmail.com>
2016-03-26 20:11:56 +0300

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 cd /vagrant/address-book
-mvn package
+mvn clean package
+sudo rm -rf /opt/wildfly/standalone/deployments/*
 sudo cp ear/target/addressbook-ear-1.0.ear /opt/wildfly/standalone/deployments
-sudo touch /opt/wildfly/standalone/deployments/addressbook-ear-1.0.ear.dodeploy
+#sudo touch /opt/wildfly/standalone/deployments/addressbook-ear-1.0.ear.dodeploy
 
