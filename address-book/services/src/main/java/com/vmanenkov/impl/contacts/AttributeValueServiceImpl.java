626e422216ff015b9b2082db055d94c9f3039bca
Valeriy Manenkov <v.manenkov@gmail.com>
2016-04-02 11:55:10 +0300

    Add AttributeValueServiceImpl

---
+++ address-book/services/src/main/java/com/vmanenkov/impl/contacts/AttributeValueServiceImpl.java
@@ -0,0 +1,40 @@
+package com.vmanenkov.impl.contacts;
+
+import com.vmanenkov.addressbook.model.contacts.AttributeValue;
+import com.vmanenkov.services.contacts.AttributeValueService;
+import com.vmanenkov.services.exceptions.AttributeValueNotFoundException;
+import com.vmanenkov.services.exceptions.AttributeValueNotValidException;
+
+import java.util.Collection;
+
+public class AttributeValueServiceImpl implements AttributeValueService {
+    // TODO: Fill AttributeValueServiceImpl::create
+    @Override
+    public AttributeValue create(String textValue, byte[] blobValue) throws AttributeValueNotValidException {
+        return null;
+    }
+
+    // TODO: Fill AttributeValueServiceImpl::get
+    @Override
+    public AttributeValue get(Long id) throws AttributeValueNotFoundException {
+        return null;
+    }
+
+    // TODO: Fill AttributeValueServiceImpl::update
+    @Override
+    public AttributeValue update(Long id, String textValue, byte[] blobValue) throws AttributeValueNotFoundException, AttributeValueNotValidException {
+        return null;
+    }
+
+    // TODO: Fill AttributeValueServiceImpl::delete
+    @Override
+    public void delete(Long id) throws AttributeValueNotFoundException {
+
+    }
+
+    // TODO: Fill AttributeValueServiceImpl::getAll
+    @Override
+    public Collection<AttributeValue> getAll() {
+        return null;
+    }
+}
