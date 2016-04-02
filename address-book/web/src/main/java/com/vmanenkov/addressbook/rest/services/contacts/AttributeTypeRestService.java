50e7e6f065c00d8fa3ae4b37e3cdfcf3c6a283c9
Valeriy Manenkov <v.manenkov@gmail.com>
2016-04-02 22:08:42 +0300

    Implements AttributeTypeRestService

--- address-book/web/src/main/java/com/vmanenkov/addressbook/rest/services/contacts/AttributeTypeRestService.java
+++ address-book/web/src/main/java/com/vmanenkov/addressbook/rest/services/contacts/AttributeTypeRestService.java
@@ -1,8 +1,16 @@
 package com.vmanenkov.addressbook.rest.services.contacts;
 
+import com.vmanenkov.addressbook.model.contacts.Attribute;
+import com.vmanenkov.addressbook.model.contacts.AttributeType;
+import com.vmanenkov.addressbook.model.contacts.FieldType;
+import com.vmanenkov.addressbook.rest.model.contacts.AttributeTypeRest;
+import com.vmanenkov.addressbook.rest.model.contacts.FieldTypeRest;
 import com.vmanenkov.profile.Profiled;
 import com.vmanenkov.services.contacts.AttributeTypeService;
+import com.vmanenkov.services.contacts.FieldTypeService;
 import com.vmanenkov.services.exceptions.AttributeTypeNotFoundException;
+import com.vmanenkov.services.exceptions.AttributeTypeNotValidException;
+import com.vmanenkov.services.exceptions.FieldTypeNotFoundException;
 import org.jboss.resteasy.annotations.cache.NoCache;
 
 import javax.enterprise.context.RequestScoped;
@@ -18,28 +26,42 @@ public class AttributeTypeRestService {
     @Inject
     private AttributeTypeService attributeTypeService;
 
+    @Inject
+    private FieldTypeService fieldTypeService;
+
     @POST
     @NoCache
     @Path("/")
-    @Produces(MediaType.APPLICATION_JSON)
-    public void createAttributeType() {
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public AttributeTypeRest createAttributeType(
+            AttributeTypeRest rest,
+            @QueryParam("field_type_id") Long fieldTypeId
+    ) throws FieldTypeNotFoundException, AttributeTypeNotValidException {
+        FieldType fieldType = fieldTypeService.get(fieldTypeId);
 
+        return convertToRest(attributeTypeService.create(rest.getName(), fieldType));
     }
 
     @GET
     @NoCache
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public void readAttributeType(@PathParam("id") Long id) {
-
+    public AttributeTypeRest readAttributeType(@PathParam("id") Long id) throws AttributeTypeNotFoundException {
+        return convertToRest(attributeTypeService.get(id));
     }
 
     @PUT
     @NoCache
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public void updateAttributeType(@PathParam("id") Long id) {
-
+    public AttributeTypeRest updateAttributeType(
+            @PathParam("id") Long id,
+            AttributeTypeRest rest,
+            @QueryParam("field_type_id") Long fieldTypeId
+    ) throws FieldTypeNotFoundException, AttributeTypeNotValidException, AttributeTypeNotFoundException {
+        FieldType fieldType = fieldTypeService.get(fieldTypeId);
+        return convertToRest(attributeTypeService.update(id, rest.getName(), fieldType));
     }
 
     @DELETE
@@ -48,5 +70,12 @@ public class AttributeTypeRestService {
     @Produces(MediaType.APPLICATION_JSON)
     public void deleteAttributeType(@PathParam("id") Long id) throws AttributeTypeNotFoundException {
         attributeTypeService.delete(id);
+    }
+
+    private AttributeTypeRest convertToRest(AttributeType attributeType) {
+        return new AttributeTypeRest(
+                attributeType.getId(),
+                attributeType.getName()
+        );
     }
 }
