23a3af146d241eec7635a3dc231a4d15de71f1e7
Valeriy Manenkov <v.manenkov@gmail.com>
2016-04-02 11:58:46 +0300

    Add FieldTypeServiceImpl stub

---
+++ address-book/services/src/main/java/com/vmanenkov/impl/contacts/FieldTypeServiceImpl.java
@@ -0,0 +1,47 @@
+package com.vmanenkov.impl.contacts;
+
+import com.vmanenkov.addressbook.model.contacts.AttributeType;
+import com.vmanenkov.addressbook.model.contacts.FieldType;
+import com.vmanenkov.services.contacts.FieldTypeService;
+import com.vmanenkov.services.exceptions.FieldTypeNotFoundException;
+import com.vmanenkov.services.exceptions.FieldTypeNotValidException;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import java.util.Collection;
+import java.util.Set;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+public class FieldTypeServiceImpl implements FieldTypeService {
+    // TODO: Fill FieldTypeServiceImpl::create
+    @Override
+    public FieldType create(String typeName, Set<AttributeType> attributeTypes) throws FieldTypeNotValidException {
+        return null;
+    }
+
+    // TODO: Fill FieldTypeServiceImpl::get
+    @Override
+    public FieldType get(Long id) throws FieldTypeNotFoundException {
+        return null;
+    }
+
+    // TODO: Fill FieldTypeServiceImpl::update
+    @Override
+    public FieldType update(Long id, String typeName, Set<AttributeType> attributeTypes) throws FieldTypeNotFoundException, FieldTypeNotValidException {
+        return null;
+    }
+
+    // TODO: Fill FieldTypeServiceImpl::delete
+    @Override
+    public void delete(Long id) throws FieldTypeNotFoundException {
+
+    }
+
+    // TODO: Fill FieldTypeServiceImpl::getAll
+    @Override
+    public Collection<FieldType> getAll() {
+        return null;
+    }
+}
