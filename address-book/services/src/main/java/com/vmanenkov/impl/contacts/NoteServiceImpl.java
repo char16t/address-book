8bcbcc132ef3eb8c01cab67394fe043dd8bd06fc
Valeriy Manenkov <v.manenkov@gmail.com>
2016-04-02 12:00:47 +0300

    Add NoteServiceImpl stub

---
+++ address-book/services/src/main/java/com/vmanenkov/impl/contacts/NoteServiceImpl.java
@@ -0,0 +1,47 @@
+package com.vmanenkov.impl.contacts;
+
+import com.vmanenkov.addressbook.model.contacts.Note;
+import com.vmanenkov.addressbook.model.contacts.Person;
+import com.vmanenkov.services.contacts.NoteService;
+import com.vmanenkov.services.exceptions.NoteNotFoundException;
+import com.vmanenkov.services.exceptions.NoteNotValidException;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import java.util.Collection;
+import java.util.Date;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+public class NoteServiceImpl implements NoteService {
+    // TODO: Fill NoteServiceImpl::create
+    @Override
+    public Note create(Date date, String value, Person person) throws NoteNotValidException {
+        return null;
+    }
+
+    // TODO: Fill NoteServiceImpl::get
+    @Override
+    public Note get(Long id) throws NoteNotFoundException {
+        return null;
+    }
+
+    // TODO: Fill NoteServiceImpl::update
+    @Override
+    public Note update(Long id, Date date, String value, Person person) throws NoteNotFoundException, NoteNotValidException {
+        return null;
+    }
+
+    // TODO: Fill NoteServiceImpl::delete
+    @Override
+    public void delete(Long id) throws NoteNotFoundException {
+
+    }
+
+    // TODO: Fill NoteServiceImpl::getAll
+    @Override
+    public Collection<Note> getAll() {
+        return null;
+    }
+}
