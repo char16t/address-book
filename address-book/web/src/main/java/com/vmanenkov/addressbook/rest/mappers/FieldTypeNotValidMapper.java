37d3e834b0ae4394491c4774ba429f953d427b34
Valeriy Manenkov <v.manenkov@gmail.com>
2016-04-02 11:31:06 +0300

    FieldTypeNotValidMapper.java

---
+++ address-book/web/src/main/java/com/vmanenkov/addressbook/rest/mappers/FieldTypeNotValidMapper.java
@@ -0,0 +1,35 @@
+package com.vmanenkov.addressbook.rest.mappers;
+
+import com.vmanenkov.addressbook.rest.model.RestError;
+import com.vmanenkov.addressbook.util.LoggerAB;
+import com.vmanenkov.services.exceptions.FieldTypeNotValidException;
+import com.vmanenkov.services.exceptions.errortypes.FieldTypeErrorType;
+
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+public class FieldTypeNotValidMapper implements ExceptionMapper<FieldTypeNotValidException> {
+
+    @Inject
+    private LoggerAB log;
+
+    @Override
+    public Response toResponse(FieldTypeNotValidException exception) {
+        Response.ResponseBuilder builder = Response.status(Response.Status.BAD_REQUEST);
+        String errorCode = null;
+        String description = null;
+        FieldTypeErrorType error = exception.getError();
+        switch (error) {
+            case FIELD_TYPE_NAME_IS_EMPTY:
+                errorCode = ApplicationErrorCodes.FIELD_TYPE_NAME_IS_EMPTY;
+                description = "Field type name should not be empty";
+                break;
+            default:
+                break;
+        }
+        RestError restError = new RestError(errorCode, description);
+        builder.entity(restError);
+        return builder.build();
+    }
+}
