f035637d8461c789f5e439b7546bbacbdf211dad
Valeriy Manenkov <v.manenkov@gmail.com>
2016-04-03 16:14:20 +0300

    Implements fieldType rest service

--- address-book/web/src/main/java/com/vmanenkov/addressbook/rest/services/contacts/FieldTypeRestService.java
+++ address-book/web/src/main/java/com/vmanenkov/addressbook/rest/services/contacts/FieldTypeRestService.java
@@ -1,8 +1,11 @@
 package com.vmanenkov.addressbook.rest.services.contacts;
 
+import com.vmanenkov.addressbook.model.contacts.FieldType;
+import com.vmanenkov.addressbook.rest.model.contacts.FieldTypeRest;
 import com.vmanenkov.profile.Profiled;
 import com.vmanenkov.services.contacts.FieldTypeService;
 import com.vmanenkov.services.exceptions.FieldTypeNotFoundException;
+import com.vmanenkov.services.exceptions.FieldTypeNotValidException;
 import org.jboss.resteasy.annotations.cache.NoCache;
 
 import javax.enterprise.context.RequestScoped;
@@ -22,24 +25,24 @@ public class FieldTypeRestService {
     @NoCache
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
-    public void createFieldType() {
-
+    public FieldTypeRest createFieldType(FieldTypeRest rest) throws FieldTypeNotValidException {
+        return convertToRest(fieldTypeService.create(rest.getTypeName()));
     }
 
     @GET
     @NoCache
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public void readFieldType(@PathParam("id") Long id) {
-
+    public FieldTypeRest readFieldType(@PathParam("id") Long id) throws FieldTypeNotFoundException {
+        return convertToRest(fieldTypeService.get(id));
     }
 
     @PUT
     @NoCache
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public void updateFieldType(@PathParam("id") Long id) {
-
+    public FieldTypeRest updateFieldType(@PathParam("id") Long id, FieldTypeRest rest) throws FieldTypeNotValidException, FieldTypeNotFoundException {
+        return convertToRest(fieldTypeService.update(id, rest.getTypeName()));
     }
 
     @DELETE
@@ -48,5 +51,12 @@ public class FieldTypeRestService {
     @Produces(MediaType.APPLICATION_JSON)
     public void deleteFieldType(@PathParam("id") Long id) throws FieldTypeNotFoundException {
         fieldTypeService.delete(id);
+    }
+
+    private FieldTypeRest convertToRest(FieldType fieldType) {
+        return new FieldTypeRest(
+                fieldType.getId(),
+                fieldType.getTypeName()
+        );
     }
 }
