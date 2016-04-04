47b58f8d11abc21957d38153fa0f4eb1ddbd936f
Sergey Efremov <efremov_su@mail.ru>
2016-04-04 14:00:29 +0300

    Fix @Produces/@Consumes annotation (NoteRestService)

--- address-book/web/src/main/java/com/vmanenkov/addressbook/rest/services/contacts/NoteRestService.java
+++ address-book/web/src/main/java/com/vmanenkov/addressbook/rest/services/contacts/NoteRestService.java
@@ -1,5 +1,6 @@
 package com.vmanenkov.addressbook.rest.services.contacts;
 
+import com.vmanenkov.addressbook.rest.model.contacts.NoteRest;
 import com.vmanenkov.profile.Profiled;
 import org.jboss.resteasy.annotations.cache.NoCache;
 
@@ -14,31 +15,32 @@ public class NoteRestService {
     @POST
     @NoCache
     @Path("/")
-    @Produces(MediaType.APPLICATION_JSON)
-    public void createNote() {
-
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public NoteRest createNote() {
+        return null;
     }
 
     @GET
     @NoCache
     @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public void readNote(@PathParam("id") Long id) {
-
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public NoteRest readNote(@PathParam("id") Long id) {
+        return null;
     }
 
     @PUT
     @NoCache
     @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public void updateNote(@PathParam("id") Long id) {
-
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public NoteRest updateNote(@PathParam("id") Long id) {
+        return null;
     }
 
     @DELETE
     @NoCache
     @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public void deleteNote(@PathParam("id") Long id) {
 
     }
