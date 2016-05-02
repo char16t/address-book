3d76d7d6615432bc5ddece0a03129e718286cfec
Valeriy Manenkov <v.manenkov@gmail.com>
2016-05-02 03:50:42 +0300

    Update initdata.yml

--- database/demodata.sql
+++ database/demodata.sql
@@ -1,22 +1,5 @@
 insert into person(id, first_name, last_name, description) values(1,'Joseph','Wilcox','Furthermore, any associated supporting element is to be regarded as the
     traditional practice of grammarians.');
-insert into attribute_group(id, name, description) values (1, 'Личная информация', 'Описание для Личная информация');
-insert into field_type(id, type_name) values (1, 'Текстовое поле');
-insert into attribute_type(id, name, field_type_id, regex, required, hidden, description) values (1, 'Свободное поле', 1, '*', 0, 0, 'Без ограничений на ввод');
-
-insert into person(id, first_name, last_name, description) values (1, 'F1', 'L1', 'F1 L1 description');
-insert into person_account(person_id, user_id) values (1, 1);
-insert into person_tag(person_id, tag_id) values (1, 1);
-
-insert into note(id, date, value) values (1, '1971-07-13', 'User 1');
-insert into person_note(person_id, note_id) values (1, 1);
-
-insert into attribute(id, name, type_id, group_id, description) values (1, 'Email', 1, 1, 'Электронная почта');
-insert into attribute(id, name, type_id, group_id, description) values (2, 'Телефон', 1, 1, 'Личный телефон');
-
-insert into attribute_value(id, person_id, attribute_id, text_value, blob_value) values (1, 1, 2, '+7(000)000-00-00', null);
-insert into attribute_value(id, person_id, attribute_id, text_value, blob_value) values (2, 1, 1, 'aaaa@mail.ru', null);
-
 insert into person(id, first_name, last_name, description) values(2,'Thomas','Marine','Comparing these examples with their parasitic gap counterparts in (96)
 and (97), we see that an important property of these three types of EC
 does not readily tolerate the ultimate standard that determines the
--- database/utils/initdata-generator/initdatagen/cli.py
+++ database/utils/initdata-generator/initdatagen/cli.py
@@ -36,6 +36,51 @@ def main():
         sql_source += "insert into tag(id, name, public_tag, description) values ({0}, '{1}', TRUE, NULL);\n".format(item_id, tag)
         item_id += 1
 
+    item_id = 1
+    ftype_ids = {}
+    for ftype in yaml_data['field_types']:
+        sql_source += "insert into field_type (id, type_name) values ({0}, '{1}');\n".format(item_id, ftype)
+        ftype_ids[ftype] = item_id
+        item_id += 1
+
+    item_id = 1
+    atype_ids = {}
+    for atype in yaml_data['attribute_types']:
+        sql_source += "insert into attribute_type (id, name, field_type_id, regex, required, hidden, description) values ({0}, '{1}', {2}, '{3}', {4}, {5}, '{6}');\n".format(item_id, atype['name'], ftype_ids[atype['type']], atype['regex'], atype['required'], atype['hidden'], atype['description'])
+        atype_ids[atype['name']] = item_id
+        item_id += 1
+
+    item_id = 1
+    attribute_ids = {}
+    for group in yaml_data['attribute_groups']:
+        sql_source += "insert into attribute_group(id, name, description) values ({0}, '{1}', '{2}');\n".format(item_id, group['name'], group['description'])
+        a_id = 1
+        for attribute in group['attributes']:
+            sql_source += "insert into attribute (id, name, type_id, group_id, description) values ({0}, '{1}', {2}, {3}, '{4}');\n".format(a_id, attribute['name'], atype_ids[attribute['type']], item_id, attribute['description'])
+            attribute_ids[attribute['name']] = a_id
+            a_id += 1
+        item_id += 1
+
+    item_id = 1
+    for p in yaml_data['persons']:
+        sql_source += "insert into person (id, first_name, last_name, description) values ({0}, '{1}', '{2}', '{3}');\n".format(item_id, p['first_name'], p['last_name'], p['description'])
+        sql_source += "insert into person_account (person_id, user_id) values ({0}, {1});\n".format(item_id, p['account'])
+
+        for tag in p['tags']:
+            sql_source += "insert into person_tag(person_id, tag_id) values ({0}, {1});\n".format(item_id, tag)
+
+        note_id = 1
+        for note in p['notes']:
+            sql_source += "insert into note (id, person_id, date, value) values ({0}, {1}, '{2}', '{3}');\n".format(note_id, item_id, note['date'], note['value'])
+            note_id += 1
+
+        info_id = 1
+        for i in p['info']:
+            sql_source += "insert into attribute_value (id, person_id, attribute_id, text_value, blob_value) values ({0}, {1}, {2}, '{3}', NULL);\n".format(info_id, item_id, attribute_ids[i['field']], i['value'])
+            info_id += 1
+
+        item_id += 1
+
     yaml_fp.close()
     print(sql_source)
 
