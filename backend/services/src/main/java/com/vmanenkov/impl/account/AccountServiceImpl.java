671d12f0d9c5dcb255b750c68be42c5f7e8aa286
kimarin <kirilychevam@gmail.com>
2016-05-18 22:46:34 +0400

    Add validating email for accounts

--- backend/services/src/main/java/com/vmanenkov/impl/account/AccountServiceImpl.java
+++ backend/services/src/main/java/com/vmanenkov/impl/account/AccountServiceImpl.java
@@ -68,8 +68,11 @@ public class AccountServiceImpl implements AccountService {
             throw new EmailNotValidException(EmailErrorType.EMAIL_IS_EMPTY);
         }
 
-        // todo: add validate
-
+        String regex = "^[-a-z0-9!#$%&*+/=?^_`{|}~]+(.[-a-z0-9!#$%&*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$";
+        //String regex = "^[a-z0-9]+@[a-z0-9]+.com$";
+        if (!email.toLowerCase().matches(regex)) {
+            throw new EmailNotValidException(EmailErrorType.EMAIL_IS_NOT_VALID);
+        }
     }
 
     @Override
--- backend/services/src/main/java/com/vmanenkov/services/exceptions/errortypes/EmailErrorType.java
+++ backend/services/src/main/java/com/vmanenkov/services/exceptions/errortypes/EmailErrorType.java
@@ -4,5 +4,5 @@ package com.vmanenkov.services.exceptions.errortypes;
 public enum EmailErrorType {
     EMAIL_IS_EMPTY,
     EMAIL_IS_NOT_UNIQUE,
-    EMAIL_IS_NOT_VALID,
+    EMAIL_IS_NOT_VALID
 }
